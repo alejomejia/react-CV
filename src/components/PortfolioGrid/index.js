f22e70b2e0d5c5199ab33568406e0721d042b028
alejomejia <alejomejiacuartas@outlook.com>
2019-10-14 14:32:03 -0500

    Change PortfolioGrid component to non-class and add Styled Components

--- src/components/PortfolioGrid/index.js
+++ src/components/PortfolioGrid/index.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
-import './style/PortfolioGrid.sass';
+import React from "react";
 
-class PortfolioGrid extends Component{
+import { Item, Head, Work, Title, Figure, Screenshot } from "./styles";
 
-  render(){
-    return(
-      <div className="portfolio-grid-item">
-        <div className="portfolio-grid-item--heading">
-          <h5 className="portfolio-grid-item--heading-work">{this.props.work}</h5>
-          <h4 className="portfolio-grid-item--heading-title">{this.props.title}</h4>
-        </div>
-        <figure className="portfolio-grid-item--screenshot">
-          <img src={this.props.screenshot} alt="Portfolio item" />
-        </figure>
-      </div>
-    )
-  }
-
+function PortfolioGrid({ work, title, screenshot }) {
+  return (
+    <Item>
+      <Head>
+        <Work>{work}</Work>
+        <Title>{title}</Title>
+      </Head>
+      <Figure>
+        <Screenshot src={screenshot} alt="Portfolio item" />
+      </Figure>
+    </Item>
+  );
 }
 
 export default PortfolioGrid;
---
+++ src/components/PortfolioGrid/styles.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import THEME from "../../config/theme";
+
+export const Item = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, calc(50% - ${THEME.spacing(1)}));
+  grid-gap: ${THEME.spacing(2)};
+
+  @media (max-width: ${THEME.breakpoints.xs}px) {
+    grid-template-columns: 100%;
+  }
+`;
+
+export const Head = styled.div`
+  display: flex;
+  margin-bottom: ${THEME.spacing(1)};
+`;
+
+export const Work = styled.h5`
+  display: inline;
+  margin: 0 ${THEME.spacing(0.5)} 0 0;
+  background: ${THEME.palette.black.main};
+  color: ${THEME.palette.white.light};
+  padding: ${THEME.spacing(0.2)} ${THEME.spacing(0.4)};
+  border-radius: 4px;
+`;
+
+export const Title = styled.h4`
+  display: inline;
+  margin: 0;
+  font-weight: 700;
+`;
+
+export const Figure = styled.figure`
+  display: block;
+  margin: 0;
+`;
+
+export const Screenshot = styled.img`
+  max-width: 100%;
+  border-radius: 5px;
+`;
