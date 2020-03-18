2e233ef1c38677b33e5238b540d0d4a987c28751
alejomejia <alejomejiacuartas@outlook.com>
2020-03-18 12:24:53 -0500

    Refactor Education information

--- src/components/Sidebar/Education/index.js
+++ src/components/Sidebar/Education/index.js
@@ -2,47 +2,21 @@ import React from "react";
 
 import { Wrapper, Title, Item, Subtitle, Description } from "./styles";
 
-import { PLATZI, SENA, ADOBE, GEEKBOSS } from "../../../config/const";
+import EDUCATION from "./data.json";
+import { getEducationInfo } from "../../../config/const";
 
 function SidebarEducation({ title }) {
   return (
     <Wrapper>
       <Title>{title}</Title>
-      <Item>
-        <Subtitle>Google Analytics & SEO {GEEKBOSS}</Subtitle>
-        <Description>
-          Certification for take the presential Google Analytics and SEO
-          training from Geekboss
-        </Description>
-      </Item>
-      <Item>
-        <Subtitle>Git and GitHub Certificate {PLATZI}</Subtitle>
-        <Description>
-          Certification for take the Git and GitHub professional course from
-          Platzi
-        </Description>
-      </Item>
-      <Item>
-        <Subtitle>User Experience & User Interface Design {PLATZI}</Subtitle>
-        <Description>
-          Certification for take User Experience and User Interface Design from
-          Platzi
-        </Description>
-      </Item>
-      <Item>
-        <Subtitle>Multimedia Production Technology {SENA}</Subtitle>
-        <Description>
-          A nicely complete technology, I've learned from graphic design and
-          illustration to web development using HTML5, CSS3 and JavaScript
-        </Description>
-      </Item>
-      <Item>
-        <Subtitle>Photoshop & Flash CS5 Certificate {ADOBE}</Subtitle>
-        <Description>
-          Certification for take Photoshop and Flash CS5 presencial evaluation
-          from Adobe
-        </Description>
-      </Item>
+      {EDUCATION.map(item => (
+        <Item key={item.id}>
+          <Subtitle>
+            {item.title} {getEducationInfo(item.name, item.url)}
+          </Subtitle>
+          <Description>{item.description}</Description>
+        </Item>
+      ))}
     </Wrapper>
   );
 }
--- src/config/const.js
+++ src/config/const.js
@@ -1,25 +1,9 @@
 import React from "react";
 
-export const PLATZI = (
-  <a href="https://platzi.com" target="_blank" rel="noopener noreferrer">
-    @Platzi
-  </a>
-);
-
-export const SENA = (
-  <a href="http://www.sena.edu.co" target="_blank" rel="noopener noreferrer">
-    @Sena
-  </a>
-);
-
-export const ADOBE = (
-  <a href="https://www.adobe.com" target="_blank" rel="noopener noreferrer">
-    @Adobe
-  </a>
-);
-
-export const GEEKBOSS = (
-  <a href="https://www.geekboss.co" target="_blank" rel="noopener noreferrer">
-    @GeekBoss
-  </a>
+export const getEducationInfo = (name, url) => (
+  <>
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      @{name}
+    </a>
+  </>
 );
