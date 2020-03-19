76e47a6f57a7f24e9d8849c642bf09ea2ae646f7
alejomejia <alejomejiacuartas@outlook.com>
2020-03-18 20:58:09 -0500

    Fix misspelled const name

--- src/components/Content/About/index.js
+++ src/components/Content/About/index.js
@@ -7,14 +7,14 @@ function ContentAbout({ title }) {
   const currentYear = date.getFullYear();
 
   const yearOfMyFirstJob = 2013;
-  const yearsOfExperince = currentYear - yearOfMyFirstJob;
+  const yearsOfExperience = currentYear - yearOfMyFirstJob;
 
   return (
     <Wrapper>
       <Title>{title}</Title>
       <p>
         I am a passionate UI.UX developer and web designer with{" "}
-        {yearsOfExperince} years of experience in different digital agencies
+        {yearsOfExperience} years of experience in different digital agencies
         from my city. I like to learn all the time about new frontend
         technologies and learn new languages.
       </p>
