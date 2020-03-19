82b86a3fa40e068e51d8b6814953dd13025d667b
alejomejia <alejomejiacuartas@outlook.com>
2020-03-18 20:55:38 -0500

    Add dinamic years of experience

--- src/components/Content/About/index.js
+++ src/components/Content/About/index.js
@@ -3,13 +3,20 @@ import React from "react";
 import { Wrapper, Title } from "./styles";
 
 function ContentAbout({ title }) {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+
+  const yearOfMyFirstJob = 2013;
+  const yearsOfExperince = currentYear - yearOfMyFirstJob;
+
   return (
     <Wrapper>
       <Title>{title}</Title>
       <p>
-        I am a passionate UI.UX developer and web designer with 6 years of
-        experience in different digital agencies from my city. I like to learn
-        all the time about new frontend technologies and learn new languages.
+        I am a passionate UI.UX developer and web designer with{" "}
+        {yearsOfExperince} years of experience in different digital agencies
+        from my city. I like to learn all the time about new frontend
+        technologies and learn new languages.
       </p>
       <p>
         In my free time, I like to play videogames, watch TV series and movies,
